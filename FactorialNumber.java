1f18450e2f2ebf66d6b10e0d7a28fcccec98c08a
shahul.hameed <shahul.hameed@host.docker.internal>
2020-01-22 18:33:23 +0530

    RecursionMethod for Factorial

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -4,6 +4,9 @@ public class FactorialNumber {
 
 	public static void main(String[] args) {
 		itervativeMethod();
+		System.out.println("==============");
+		int value=recursionFactorial(5);
+		System.out.println(value);
 	}
 
 	public static void itervativeMethod() {
@@ -17,5 +20,20 @@ public class FactorialNumber {
 
 		}
 		System.out.println(fact);
+	}
+	
+	
+	
+	public static int  recursionFactorial(int num)
+	{
+		
+		int fact=1;
+		
+		if(num>=1)
+		
+			return num * recursionFactorial(num-1);
+		else
+			return 1;
+		
 	}
 }
